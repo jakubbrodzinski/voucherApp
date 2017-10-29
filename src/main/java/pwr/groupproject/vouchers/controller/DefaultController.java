c8ae78d1fef13701ad419d178e2e80b00a196310
arekziobrowski <arekziobrowski@gmail.com>
2017-10-29 07:55:53 +0100

    #14 added default Controller for home page #23 added end-state Webflow transition from cancel to "/"

---
+++ src/main/java/pwr/groupproject/vouchers/controller/DefaultController.java
@@ -0,0 +1,13 @@
+package pwr.groupproject.vouchers.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class DefaultController {
+
+    @RequestMapping("/")
+    public String home() {
+        return "index";
+    }
+}

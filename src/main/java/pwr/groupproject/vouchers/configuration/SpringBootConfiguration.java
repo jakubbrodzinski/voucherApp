c246d412e261029a59376d5f27a3972e2deb82b4
jakubbrodzinski <jakub.brodzinski@yahoo.com>
2017-12-02 13:31:33 +0100

    http -> https redirection

--- src/main/java/pwr/groupproject/vouchers/configuration/SpringBootConfiguration.java
+++ src/main/java/pwr/groupproject/vouchers/configuration/SpringBootConfiguration.java
@@ -1,12 +1,55 @@
 package pwr.groupproject.vouchers.configuration;
 
+import org.apache.catalina.Context;
+import org.apache.catalina.connector.Connector;
+import org.apache.tomcat.util.descriptor.web.SecurityCollection;
+import org.apache.tomcat.util.descriptor.web.SecurityConstraint;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.embedded.EmbeddedServletContainerFactory;
+import org.springframework.boot.context.embedded.tomcat.TomcatEmbeddedServletContainerFactory;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication(scanBasePackages = {"pwr.groupproject.vouchers.configuration","pwr.groupproject.vouchers.dao","pwr.groupproject.vouchers.services","pwr.groupproject.vouchers.controller"})
 public class SpringBootConfiguration {
 
     public static void main(String[] args){
         SpringApplication.run(SpringBootConfiguration.class);
+    }
+
+    @Bean
+    public EmbeddedServletContainerFactory servletContainer() {
+        TomcatEmbeddedServletContainerFactory tomcat =
+                new TomcatEmbeddedServletContainerFactory() {
+
+                    @Override
+                    protected void postProcessContext(Context context) {
+                        SecurityConstraint securityConstraint = new SecurityConstraint();
+                        securityConstraint.setUserConstraint("CONFIDENTIAL");
+                        SecurityCollection collection = new SecurityCollection();
+                        collection.addPattern("/*");
+                        securityConstraint.addCollection(collection);
+                        context.addConstraint(securityConstraint);
+                    }
+                };
+        tomcat.addAdditionalTomcatConnectors(createHttpConnector());
+        return tomcat;
+    }
+
+    @Value("${server.port.http}")
+    private int serverPortHttp;
+
+    @Value("${server.port}")
+    private int serverPortHttps;
+
+    private Connector createHttpConnector() {
+        Connector connector =
+                new Connector("org.apache.coyote.http11.Http11NioProtocol");
+        connector.setScheme("http");
+        connector.setSecure(false);
+        connector.setPort(serverPortHttp);
+        connector.setRedirectPort(serverPortHttps);
+        return connector;
     }
 }
